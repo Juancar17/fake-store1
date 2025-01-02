fc859be6dad9e2e49d04d8982ec90ed450d736c9
Juan Carlos Baez Diaz <146070006+Juancar17@users.noreply.github.com>
2025-01-02 17:18:54 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import NavBar from "./components/NavBar.jsx";
-import Hero from "./components/hero.jsx";
+import Hero from "./components/Hero.jsx";
 import Categories from "./components/Categories.jsx";
 import ProductCarousel from "./components/ProductCarousel.jsx";
 import Jewelery from "./components/Jewelery.jsx";
